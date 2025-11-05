d3fc4a1021e4cab6ef08ecdc345ce0f8f1717bb3
Arnab Singha Roy <arnabsr0702@gmail.com>
2025-11-05 10:37:04 +0530

    apps pending resolved

--- frontend/src/components/AppsPending.jsx
+++ frontend/src/components/AppsPending.jsx
@@ -51,48 +51,48 @@ const AppsPending = () => {
             </div>
             <div className='bg-cyan-950 flex justify-center items-center gap-1 md:gap-8 py-2 px-1 md:px-5'>
 
-                <input type="text" className='w-22 md:w-40 bg-cyan-800 rounded-md px-2 py-2 text-gray-400' placeholder='Filter by name...' />
-                <input type="text" className='w-22 md:w-40 bg-cyan-800 rounded-md px-2 py-2 text-gray-400' placeholder='Filter by vendor...' />
-                <input type="text" className='w-22 md:w-40 bg-cyan-800 rounded-md px-2 py-2 text-gray-400' placeholder='Filter by name...' />
-                <h5 className='w-full text-gray-400 text-center'>3 results</h5>
+                <input type="text" className='w-20 md:w-40 bg-cyan-800 rounded-md sm:px-2 py-2 text-gray-400' placeholder='Filter by name...' />
+                <input type="text" className='w-20 md:w-40 bg-cyan-800 rounded-md sm:px-2 py-2 text-gray-400' placeholder='Filter by vendor...' />
+                <input type="text" className='w-20 md:w-40 bg-cyan-800 rounded-md sm:px-2 py-2 text-gray-400' placeholder='Filter by name...' />
+                <h5 className='w-22 md:w-full text-gray-400 text-center'>3 results</h5>
             </div >
 
             <div className='flex flex-col gap-6'>
 
-                <div className='flex items-center justify-start  gap-4 px-2 md:px-8'>
+                <div className='flex items-center justify-start gap-1.5 sm:gap-4 px-2 md:px-8'>
                     <h5 className='text-white sm:font-bold w-20 sm:w-44'>Design Studio</h5>
                     <p className='text-gray-300 w-20 sm:w-44'>Creative Labs</p>
                     <p className='text-gray-300 w-44 text-center'>22 Oct, 2025</p>
                     <div className='flex flex-col xl:flex-row items-center justify-center gap-2 lg:gap-5 ps-3 py-2'>
-                        <div className='bg-blue-800 text-blue-300 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-xs lg:text-sm'><FaRegEye className='h-3 w-3 sm:h-4 sm:w-5'/>View</div>
-                        <div className='bg-green-800 text-green-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-xs  lg:text-sm'><TiTickOutline className='h-3 w-3 sm:h-4 sm:w-5'/>Approve</div>
-                        <div className='bg-yellow-800 text-yellow-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-xs  lg:text-sm'><IoAlertCircleOutline className='h-3 w-3 sm:h-4 sm:w-5'/>Request Changes</div>
-                        <div className='bg-red-800 text-red-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-xs lg:text-sm'><GiCancel className='h-3 w-3 sm:h-4 sm:w-5'/>Reject</div>
+                        <div className='bg-blue-800 text-blue-300 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-xs lg:text-sm'><FaRegEye className='h-0 w-0 sm:h-4 sm:w-5'/>View</div>
+                        <div className='bg-green-800 text-green-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-xs  lg:text-sm'><TiTickOutline className='h-0 w-0 sm:h-4 sm:w-5'/>Approve</div>
+                        <div className='bg-yellow-800 text-yellow-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-xs  lg:text-sm'><IoAlertCircleOutline className='h-0 w-0 sm:h-4 sm:w-5'/>Request Changes</div>
+                        <div className='bg-red-800 text-red-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-xs lg:text-sm'><GiCancel className='h-0 w-0 sm:h-4 sm:w-5'/>Reject</div>
                     </div>
                     
                 </div>
 
-                <div className='flex items-center justify-start  gap-4 px-2 md:px-8'>
+                <div className='flex items-center justify-start gap-1.5 sm:gap-4 px-2 md:px-8'>
                     <h5 className='text-white sm:font-bold w-20 sm:w-44'>SecureVault VPN</h5>
                     <p className='text-gray-300 w-20 sm:w-44'>Privacy Solutions</p>
                     <p className='text-gray-300 w-44 text-center'>22 Oct, 2025</p>
                     <div className='flex flex-col xl:flex-row items-center justify-center gap-2 lg:gap-5 ps-3 py-2'>
-                        <div className='bg-blue-800 text-blue-300 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-sm'><FaRegEye className='h-3 w-3 sm:h-4 sm:w-5'/>View</div>
-                        <div className='bg-green-800 text-green-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-sm'><TiTickOutline className='h-3 w-3 sm:h-4 sm:w-5'/>Approve</div>
-                        <div className='bg-yellow-800 text-yellow-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-sm'><IoAlertCircleOutline className='h-3 w-3 sm:h-4 sm:w-5'/>Request Changes</div>
-                        <div className='bg-red-800 text-red-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-sm'><GiCancel className='h-3 w-3 sm:h-4 sm:w-5'/>Reject</div>
+                        <div className='bg-blue-800 text-blue-300 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-sm'><FaRegEye className='h-0 w-0 sm:h-4 sm:w-5'/>View</div>
+                        <div className='bg-green-800 text-green-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-sm'><TiTickOutline className='h-0 w-0  sm:h-4 sm:w-5'/>Approve</div>
+                        <div className='bg-yellow-800 text-yellow-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-sm'><IoAlertCircleOutline className='h-0 w-0  sm:h-4 sm:w-5'/>Request Changes</div>
+                        <div className='bg-red-800 text-red-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-sm'><GiCancel className='h-0 w-0  sm:h-4 sm:w-5'/>Reject</div>
                     </div>
                 </div>
 
-                <div className='flex items-center justify-start  gap-4 px-2 md:px-8'>
+                <div className='flex items-center justify-start gap-1.5 sm:gap-4 px-2 md:px-8'>
                     <h5 className='text-white sm:font-bold w-20 sm:w-44'>TaskMaster Pro</h5>
                     <p className='text-gray-300 w-20 sm:w-44'>Productivity Co.</p>
                     <p className='text-gray-300 w-44 text-center'>22 Oct, 2025</p>
                     <div className='flex flex-col xl:flex-row items-center justify-center gap-2 lg:gap-5 ps-3 py-2'>
-                        <div className='bg-blue-800 text-blue-300 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-sm'><FaRegEye className='h-3 w-3 sm:h-4 sm:w-5'/>View</div>
-                        <div className='bg-green-800 text-green-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-sm'><TiTickOutline className='h-3 w-3 sm:h-4 sm:w-5'/>Approve</div>
-                        <div className='bg-yellow-800 text-yellow-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-sm'><IoAlertCircleOutline className='h-3 w-3 sm:h-4 sm:w-5'/>Request Changes</div>
-                        <div className='bg-red-800 text-red-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-sm'><GiCancel className='h-3 w-3 sm:h-4 sm:w-5'/>Reject</div>
+                        <div className='bg-blue-800 text-blue-300 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-sm'><FaRegEye className='h-0 w-0 sm:h-4 sm:w-5'/>View</div>
+                        <div className='bg-green-800 text-green-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-sm'><TiTickOutline className='h-0 w-0 sm:h-4 sm:w-5'/>Approve</div>
+                        <div className='bg-yellow-800 text-yellow-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-sm'><IoAlertCircleOutline className='h-0 w-0 sm:h-4 sm:w-5'/>Request Changes</div>
+                        <div className='bg-red-800 text-red-500 flex justify-center items-center sm:gap-2 rounded-md sm:rounded-full px-2 py-1 text-sm'><GiCancel className='h-0 w-0 sm:h-4 sm:w-5'/>Reject</div>
                     </div>
                     
                 </div>
